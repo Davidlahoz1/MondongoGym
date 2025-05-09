4e5b91628b6274f0ee62cbcf3b962b49795d2dd0
Davidlahoz1 <david.lahozboyero@cieep.com>
2025-05-09 13:47:12 +0200

    se ha puesto tontorron la web

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ErrorComponent } from './components/error/error.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ClasesComponent } from './components/clases/clases.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component:InicioComponent
+    },
+    {
+        path: 'perfil',
+        component: PerfilComponent
+    },
+   
+    {
+        path: 'clases',
+        component: ClasesComponent
+    },
+    
+    {
+        path: '**',
+        component: ErrorComponent,
+        pathMatch: 'full'
+    }
+];
