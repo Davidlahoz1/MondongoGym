72e3036ba6bb162b61002816ab2f54a7f8af598e
Davidlahoz1 <david.lahozboyero@cieep.com>
2025-05-09 16:21:50 +0200

    tt

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+   email: string = '';
+  password: string = '';
+  loading: boolean = false;
+  hover: boolean = false;
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    this.loading = true;
+    
+    // Simular autenticación
+    setTimeout(() => {
+      this.router.navigate(['/home']);
+    }, 1500);
+  }
 }
