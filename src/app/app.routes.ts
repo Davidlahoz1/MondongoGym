c471adbf4d07d2931d0cfcd6633a33ef7c931cc9
Davidlahoz1 <david.lahozboyero@cieep.com>
2025-05-09 15:24:30 +0200

    finalizado parte david

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { ErrorComponent } from './components/error/error.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { ClasesComponent } from './components/clases/clases.component';
+import { HorarioComponent } from './components/horario/horario.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 export const routes: Routes = [
     {
@@ -23,7 +25,18 @@ export const routes: Routes = [
         path: 'clases',
         component: ClasesComponent
     },
-    
+    {
+        path: 'clases/:id',
+        component: ClasesComponent
+    },
+    {
+        path: 'horarios',
+        component: HorarioComponent
+    },
+    {
+        path: 'contacto',
+        component: ContactoComponent
+    },
     {
         path: '**',
         component: ErrorComponent,
